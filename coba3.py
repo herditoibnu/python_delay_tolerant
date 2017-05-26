f32ce21f6beee2235ac99390e5e0a14794574a56
DESKTOP-FFINH2R\herdito <herditoibnu@gmail.com>
2017-05-26 13:49:19 +0700

    dest_recv

---
+++ coba3.py
@@ -0,0 +1,34 @@
+import socket
+import struct
+import threading
+import pickle
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+client.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(('', MCAST_PORT))
+
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+server.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+def sendfeedback():
+    while True:
+        msg = server.recv(10240)
+        msg = pickle.loads(msg)
+        print msg[0]
+        client.sendto("pesan diterima", (MCAST_GRP, MCAST_PORT))
+        break
+
+threads = []
+t_sendfeedback = threading.Thread(target=sendfeedback)
+threads.append(t_sendfeedback)
+t_sendfeedback.start()
+
+while True:
+    if not t_sendfeedback.is_alive():
+        break
